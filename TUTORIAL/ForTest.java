89dd2f963fbb7e9de442c9a3b3228dc38b862d24
maruyu31 <you3746336@gmail.com>
2021-11-19 17:30:56 +0900

    [Finish] ブロック

--- TUTORIAL/ForTest.java
+++ TUTORIAL/ForTest.java
@@ -1,13 +1,21 @@
 public class ForTest {
   public static void main(String[] args) {
     for (int i = 1; i <= 10; i++) {
-      // int n = i + 1;
-      // System.out.println("繰り返し:" + n + "週目");
       if (i == 5) {
         System.out.println("次のループに進む");
         continue;
       }
       System.out.println("繰り返し:" + i + "週目");
     }
+    int k = 1;
+    int l = 5;
+    System.out.println("kの初期値" + k);
+    while(k <= 5) {
+      System.out.println("while繰り返し:" + k + "週目。lの値:" + l);
+      k++;
+    }
+    System.out.println("ブロックを抜けた後のlの値" + l); // スコープ外！
+    System.out.println("プログラムの終了");
+    System.out.println("kの終了値" + k);
   }
 }
