bede6ff405c741d73bbb583fcbd0005ed108e5b6
maruyu31 <you3746336@gmail.com>
2021-11-20 21:49:45 +0900

    [Start] Package

---
+++ TUTORIAL/jp/co/funatz/pkgTest/PackageTest.java
@@ -0,0 +1,8 @@
+package jp.co.funatz.pkgTest;
+
+public class PackageTest {
+  public static void main(String[] args) {
+    jp.co.funatz.pkgTest.a.PackageA.a();
+    jp.co.funatz.pkgTest.b.PackageB.b();
+  }
+}
